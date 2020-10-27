e583412497ece595b39b4d7a9d6de11fb0a27b47
nicolas <nicolasm15@hotmail.com>
2020-10-27 16:04:16 -0300

    feat: add mobile with router navigation and map

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -10,7 +10,7 @@ const routes = Router();
 
 const upload = multer(uploadConfig);
 
-routes.post('/orphanages', upload.array('images') ,OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 
